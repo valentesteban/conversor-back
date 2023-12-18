2fd2d25ece716ed971f6b0adf02b6b96c01b9b4b
Esteban Valente <valente.estebann@gmail.com>
2023-12-18 17:39:20 -0300

    feat: created the plan service interface

---
+++ Data/Interfaces/IPlanService.cs
@@ -0,0 +1,15 @@
+using conversor.Entities;
+
+namespace conversor.Data.Interfaces;
+
+public interface IPlanService
+{
+    public List<Plan> GetPlans();
+    
+    public Plan? GetPlanId(int id);
+    public Plan? GetPlanName(string name);
+    
+    public Plan CreatePlan(Plan planForCreationDto);
+    public void UpdatePlan(Plan planForUpdateDto);
+    public void DeletePlan(int planId);
+}
