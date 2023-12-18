9e3b882ef164b35493471676262e5d839b50a292
Esteban Valente <valente.estebann@gmail.com>
2023-12-18 19:50:45 -0300

    feat: created the exchange service interface

---
+++ Data/Interfaces/IExchangeService.cs
@@ -0,0 +1,13 @@
+using conversor.Entities;
+using conversor.Models;
+
+namespace conversor.Data.Interfaces;
+
+public interface IExchangeService
+{
+    public List<CoinExchange> GetExchanges();
+    public List<CoinExchange> GetExchangesFromUser(int userId, int limit);
+    public CoinExchange CreateExchange(ExchangeForCreationDTO exchangeForCreationDto);
+    public List<CoinExchange> GeExchangesByPage(int page);
+    public int GetTotalExchanges();
+}
