8c6adfcc39010ad87a51e56c8b1a1f93ebd07bc3
Esteban Valente <valente.estebann@gmail.com>
2023-12-18 19:51:56 -0300

    chore: updated from coin to coinExchange

--- Entities/CoinExchange.cs
+++ Entities/CoinExchange.cs
@@ -8,10 +8,10 @@ public class CoinExchange
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     [ManyToOne (CascadeOperations = CascadeOperation.CascadeRead)]
-    public Coin FromCoin { get; set; } = null!;
+    public CoinExchange FromCoin { get; set; } = null!;
 
     [ManyToOne (CascadeOperations = CascadeOperation.CascadeRead)]
-    public Coin ToCoin { get; set; } = null!;
+    public CoinExchange ToCoin { get; set; } = null!;
     public DateTime Date { get; set; }
     public double Amount { get; set; }
     [ForeignKey(typeof(User))]
