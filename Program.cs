b21d2fecf7396f07d9fd2ab73bda580be8ee684d
Esteban Valente <valente.estebann@gmail.com>
2023-12-19 01:40:29 -0300

    feat: added the CoinService in the main class

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ConversorContext>();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICoinService, CoinService>();
 builder.Services.AddScoped<IExchangeService, ExchangeService>();
 builder.Services.AddScoped<IPlanService, PlanService>();
 builder.Services.AddScoped<IUserService, UserService>();
