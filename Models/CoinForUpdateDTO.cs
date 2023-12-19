f0eb3c2962b079773870bb480418172847390213
Esteban Valente <valente.estebann@gmail.com>
2023-12-19 01:37:34 -0300

    Update CoinForUpdateDTO.cs

--- Models/CoinForUpdateDTO.cs
+++ Models/CoinForUpdateDTO.cs
@@ -2,7 +2,7 @@ namespace conversor.Models;
 
 public class CoinForUpdateDTO
 {
-    public int CurrencyId { get; set; }
+    public int CoinId { get; set; }
     public string Name { get; set; }
     public string Code { get; set; }
     public double Value { get; set; }
