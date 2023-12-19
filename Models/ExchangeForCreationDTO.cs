60991aff667d394122b9017d1baca6f577001fbf
Esteban Valente <valente.estebann@gmail.com>
2023-12-19 01:37:29 -0300

    Update ExchangeForCreationDTO.cs

--- Models/ExchangeForCreationDTO.cs
+++ Models/ExchangeForCreationDTO.cs
@@ -3,7 +3,7 @@ namespace conversor.Models;
 public class ExchangeForCreationDTO
 {
     public int UserId { get; set; }
-    public int FromCurrencyId { get; set; }
-    public int ToCurrencyId { get; set; }
+    public int FromCoinId { get; set; }
+    public int ToCoinId { get; set; }
     public double Amount { get; set; }
 }
