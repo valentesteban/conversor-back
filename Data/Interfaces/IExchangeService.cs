ce7c9c2ee1fcd61ce3bc9871f4c37ff0787b05f9
Esteban Valente <valente.estebann@gmail.com>
2023-12-19 01:38:54 -0300

    chore: fixed some spelling mistakes

--- Data/Interfaces/IExchangeService.cs
+++ Data/Interfaces/IExchangeService.cs
@@ -8,6 +8,6 @@ public interface IExchangeService
     public List<CoinExchange> GetExchanges();
     public List<CoinExchange> GetExchangesFromUser(int userId, int limit);
     public CoinExchange CreateExchange(ExchangeForCreationDTO exchangeForCreationDto);
-    public List<CoinExchange> GeExchangesByPage(int page);
+    public List<CoinExchange> GetExchangesByPage(int page);
     public int GetTotalExchanges();
 }
