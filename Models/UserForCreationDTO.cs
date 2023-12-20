c7bba59e54e54f269b5104f0d452c1fb5477776b
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 10:41:56 -0300

    Update UserForCreationDTO.cs

--- Models/UserForCreationDTO.cs
+++ Models/UserForCreationDTO.cs
@@ -5,7 +5,7 @@ namespace conversor.Models;
 
 public class UserForCreationDTO
 {
-    public String UserName { get; set; }
+    public String Username { get; set; }
     public String FirstName { get; set; }
     public String LastName { get; set; }
     public String Email { get; set; }
