7343542f107031507287ea24e862a8ab7b02fb7d
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 11:13:21 -0300

    chore: change username for mail

--- Models/UserForLoginDTO.cs
+++ Models/UserForLoginDTO.cs
@@ -2,6 +2,6 @@ namespace conversor.Models;
 
 public class UserForLoginDTO
 {
-    public string Username { get; set; }
+    public string Email { get; set; }
     public string Password { get; set; }
 }
