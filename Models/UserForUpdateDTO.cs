39eeb91dca0b1ddb178c001ba3b548b318003e49
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 10:42:00 -0300

    Update UserForUpdateDTO.cs

--- Models/UserForUpdateDTO.cs
+++ Models/UserForUpdateDTO.cs
@@ -2,8 +2,8 @@
 
 public class UserForUpdateDTO
 {
-    public int UserToChangeID { get; set; }
-    public String UserName { get; set; }
+    public int UserId { get; set; }
+    public String Username { get; set; }
     public String FirstName { get; set; }
     public String LastName { get; set; }
     public String Email { get; set; }
