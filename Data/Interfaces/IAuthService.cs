316b4f95afb25b50132d70d63bff8905988a073b
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 10:40:51 -0300

    Update IAuthService.cs

--- Data/Interfaces/IAuthService.cs
+++ Data/Interfaces/IAuthService.cs
@@ -7,6 +7,6 @@ public interface IAuthService
 {
     public Auth Authenticate(UserForLoginDTO userForLoginDto);
     public string GenerateToken(Auth auth);
-    public Auth getCurrentUser();
-    public Boolean isSameUserRequest(int userId);
+    public Auth GetCurrentUser();
+    public Boolean SameUserRequest(int userId);
 }
