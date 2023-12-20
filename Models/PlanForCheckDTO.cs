15d07eab6d5f373ccba5776a8eb68a8ae04d45aa
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 10:41:42 -0300

    feat: create new view dto

---
+++ Models/PlanForCheckDTO.cs
@@ -0,0 +1,9 @@
+namespace conversor.Models;
+
+public class PlanForCheckDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Limit { get; set; }
+    public double Price { get; set; }
+}
